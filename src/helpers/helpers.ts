5b129439f9fd68523f57895af91c89031de34d79
blackparadise0407 <blackparadise0407@gmail.com>
2022-08-20 23:37:30 +0700

    Add helpers

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -56,3 +56,9 @@ export const generateBoard = (
     minesCords,
   }
 }
+
+export const isMobile = () => {
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+    navigator.userAgent,
+  )
+}
